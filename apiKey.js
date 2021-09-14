c6d4673fb5692a4a7b7b805b0fbd711a601714b6
ocodner-7 <odaine.codner@gmail.com>
2021-09-14 19:16:09 +0100

    Hidden2

---
+++ apiKey.js
@@ -0,0 +1,4 @@
+let apiKey = '04c35731a5ee918f014970082a0088b1&page=1'
+
+export default apiKey;
+
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const apiURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+import apiKey from "./apiKey";
+const apiURL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}`;
 const imagePack = 'https://image.tmdb.org/t/p/w1280';
 const main = document.getElementById('main');
 const form = document.getElementById('form');
