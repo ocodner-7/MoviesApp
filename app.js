6445840e355f9773efb8d2de5a23b2ee4f36c225
ocodner-7 <odaine.codner@gmail.com>
2021-09-15 17:04:29 +0100

    Working

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-import apiKey from "./.env";
-const apiURL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}`;
+const apiURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
 const imagePack = 'https://image.tmdb.org/t/p/w1280';
 const main = document.getElementById('main');
 const form = document.getElementById('form');
